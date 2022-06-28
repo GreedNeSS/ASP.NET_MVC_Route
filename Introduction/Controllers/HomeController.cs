8a13aa6588c76695b7f94f7b8a4b48561999ee2b
GreedNeSS <ruslan199126@gmail.com>
2022-06-28 20:53:18 +0300

    Added new example.

---
+++ Introduction/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Introduction.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            var controller = RouteData.Values["controller"];
+            var action = RouteData.Values["action"];
+            return $"Home Page!\nController: {controller}; Action: {action}";
+        }
+
+        public string Error()
+        {
+            return "Error Page!";
+        }
+    }
+}
--- Introduction/Program.cs
+++ Introduction/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapDefaultControllerRoute();
+app.MapFallbackToController("Error", "Home");
 
 app.Run();
