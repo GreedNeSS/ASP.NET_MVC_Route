be43fa7312b13b65d30afa79480111ddadddae8b
GreedNeSS <ruslan199126@gmail.com>
2022-06-30 22:54:40 +0300

    Added routing attribute example.

---
+++ Attributes/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Attributes.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("{controller=Home}/{action=Index}/{Id:int?}")]
+        public IActionResult Index(int? id)
+        {
+            string result = "Home Page!";
+
+            if (id is not null)
+            {
+                result += $"\nId: {id}";
+            }
+
+            return Content(result);
+        }
+
+        [Route("contacts")]
+        [Route("about")]
+        public string About()
+        {
+            return "About Page";
+        }
+    }
+}
---
+++ Attributes/Controllers/InfoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Attributes.Controllers
+{
+    [Route("Info")]
+    public class InfoController : Controller
+    {
+        [Route("{name:minlength(3)}/{age:int}")]
+        public IActionResult Person(string name, int age)
+        {
+            return Content($"Name: {name}, Age: {age}");
+        }
+
+        [Route("act/rout/{id=223}")]
+        public string Act(int? id)
+        {
+            return $"Id = {id}";
+        }
+    }
+}
